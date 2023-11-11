9c7a834a3abb23f27309c949932f0e247041035a
Archit Kandu <architkandu@hotmail.com>
2023-11-11 18:41:59 +0530

    Colours and Background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-  return <div>Hello, World!</div>
+  const classes = useStyles();
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
